4bc7707db60954cd36f15c2da348199db978e995
Julián Botero <juanbo97@hotmail.com>
2019-11-17 18:59:37 -0500

    Creación de Componentes, Clases y Servicios.
    Se crean todas las clases, servicios y componentes a usar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { PrincipalComponent } from './componentes/partes/principal/principal.component';
+import { LoginComponent } from './componentes/partes/login/login.component';
+import { BuscarVehiculoComponent } from './componentes/partes/buscar-vehiculo/buscar-vehiculo.component';
+import { ListarVehiculosComponent } from './componentes/partes/listar-vehiculos/listar-vehiculos.component';
+import { IngresarVehiculoComponent } from './componentes/partes/ingresar-vehiculo/ingresar-vehiculo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    PrincipalComponent,
+    LoginComponent,
+    BuscarVehiculoComponent,
+    ListarVehiculosComponent,
+    IngresarVehiculoComponent
   ],
   imports: [
     BrowserModule,
