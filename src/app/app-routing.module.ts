d2ac87492f5c92db240fdad918276664907c406f
Julián Botero <juanbo97@hotmail.com>
2019-11-17 22:27:45 -0500

    Angular Routing & Header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './componentes/partes/login/login.component';
+import { BuscarVehiculoComponent } from './componentes/partes/buscar-vehiculo/buscar-vehiculo.component';
+import { ListarVehiculosComponent } from './componentes/partes/listar-vehiculos/listar-vehiculos.component';
+import { IngresarVehiculoComponent } from './componentes/partes/ingresar-vehiculo/ingresar-vehiculo.component';
+import { PrincipalComponent } from './componentes/partes/principal/principal.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+    {path: 'buscar-vehiculo', component:  BuscarVehiculoComponent},
+    {path: 'listar-vehiculos', component: ListarVehiculosComponent},
+    {path: 'ingresar-vehiculo', component: IngresarVehiculoComponent},
+    {path: 'principal', component: PrincipalComponent},
+    {path: '**', pathMatch: 'full', redirectTo: 'login'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
